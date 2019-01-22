29881d1619d42a3d031054aa3e7f7fd1e1e407c0
sandeep kumar <absandeepsa@gmail.com>
2019-01-22 10:04:09 +0530

    update Pattern2.java
    code of Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -1,1 +1,18 @@
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Pattern2
+{
+	public static void main (String[] args)
+	{
+        for (int i=0;i<=5 ;i++ ){
+            for (int j=5;j>=i ;j-- ){
+                System.out.print("*");
+            } 
+            System.out.println();
+        } 	
+	}
+}
